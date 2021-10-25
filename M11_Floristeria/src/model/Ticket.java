0c91757d17c2d5e9e794bba682bfe2a61675994b
yalipct <aliuvys5@gmail.com>
2021-10-25 18:41:40 +0200

    modificada clase ticket

--- M11_Floristeria/src/model/Ticket.java
+++ M11_Floristeria/src/model/Ticket.java
@@ -1,40 +1,58 @@
 package model;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import controller.CampoVacio;
+
 public class Ticket {
 
-	List<Producto> productosTicket = new ArrayList<Producto>();
-	int id; //
-	static int counter = 1;
-	LocalDate date;
+	private int id;
+	private static int counter = 1;
+	private LocalDateTime date;
+	private List<Producto> productosTicket = new ArrayList<Producto>();
 	double totalCount;
-	
-	   public Ticket() {
-		
-	   this.id = counter++;
-	   this.date = LocalDate.now();   
-	  
-	   }
-		
-	public int getId () {
-		return id;
-	}	
-	
-	public void addProduct(Producto producto) {
-		productosTicket.add(producto);
-	}	
-	
-	public double getTotalPrice() {
-	
-	  totalCount = 0;	
-	
-      productosTicket.forEach(p -> totalCount += p.getPrice());
 
-	  return totalCount;
-	  
+	/* formato de la fecha */
+	private DateTimeFormatter format = DateTimeFormatter.ofPattern("'FECHA:' dd/MM/yyyy	'HORA:' HH:mm");
+
+	public Ticket(List<Producto> productosTicket) throws CampoVacio {
+
+		if (productosTicket.isEmpty()) {
+			throw new CampoVacio("Debe haber comprado al menos un producto para generar un ticket");
+		}
+		this.id = counter++;
+		this.date = LocalDateTime.now();
+
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void addProduct(Producto producto) {
+		productosTicket.add(producto);
+	}
+
+	public double getTotalPrice() {
+
+		totalCount = 0;
+
+		productosTicket.forEach(p -> totalCount += p.getPrice());
+
+		return totalCount;
+
+	}
+
+	@Override
+	public String toString() {
+		return "Ticket [id=" + id + ", date=" + date.format(format) + ", productosTicket=" + productosTicket
+				+ ", totalCount=" + totalCount + "]";
+	}
+
+	public void imprimirTicket() {
+		productosTicket.forEach(System.out::println);
+	}
 }
